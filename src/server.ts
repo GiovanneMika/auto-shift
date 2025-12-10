a9d3b6c2e024c2856ed98002d23aee7a39cfe844
Giovanne-Vamilly <mika.2023@alunos.utfpr.edu.br>
2025-12-10 14:04:28 -0300

    Inicio do projeto

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+
+import { categoriesRoutes } from './routes/categories.routes';
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3333, () => console.log('Server is running on port 3333'));
+
+app.post('/courses', (request, response)=> {
+    const {name} = request.body;
+    return response.status(201).json({
+        message: name,
+    })
+
+})
+
+app.get("/", (request, response)=> {
+    return response.json({message: "Hello World"});
+});
+
+app.use("/categories", categoriesRoutes);
