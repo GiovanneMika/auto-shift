a7757648deb96821a493cba4afa0467211f6880b
Giovanne-Vamilly <mika.2023@alunos.utfpr.edu.br>
2025-12-12 11:03:30 -0300

    documentação com swagger e readme

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,21 @@
-import express from 'express';
+import express from "express";
 
-import { router } from './routes';
+import { router } from "./routes";
+
+import swaggerUI from 'swagger-ui-express';
+
+import swaggerFile from "./swagger.json"
 
 const app = express();
 
 app.use(express.json());
 
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
+
 app.listen(3333, () => console.log('Server is running on port 3333'));
 
-app.get("/", (request, response)=> {
-    return response.json({message: "Hello World"});
+app.get("/", (request, response) => {
+    return response.json({ message: "Hello World" });
 });
 
 app.use(router);
