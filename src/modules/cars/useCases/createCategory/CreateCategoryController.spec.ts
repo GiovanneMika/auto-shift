72779389bdef635a8bb443bd1b473fae261307bf
Giovanne-Vamilly <mika.2023@alunos.utfpr.edu.br>
2025-12-22 14:17:03 -0300

    chore: adiciona teste para controller de criar categoria

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -0,0 +1,77 @@
+import { app } from "@shared/infra/http/app";
+import request from "supertest";
+import createConnection from "@shared/infra/typeorm";
+import { Connection } from "typeorm";
+import { v4 as uuidv4 } from "uuid";
+import { hash } from "bcrypt";
+
+let connection: Connection;
+
+
+
+describe("Create Category Controller", () => {
+
+    beforeAll(async () => {
+        connection = await createConnection();
+        await connection.runMigrations();
+        const id = uuidv4();
+        const password = await hash("admin", 8);
+        await connection.query(
+            `INSERT INTO USERS (id, name, email, password, "isAdmin", created_at, driver_license) values('${id}', 'admin', 'admin@email.com', '${password}', true, 'now()', 'XXXXX-XXXX')`
+        );
+    });
+
+    afterAll(async () => {
+        await new Promise(resolve => setTimeout(resolve, 500));
+        await connection.dropDatabase();
+        await connection.close();
+    });
+
+    it("should be able to create a new category", async () => {
+        const responseToken = await request(app).post("/sessions").send({
+            email: "admin@email.com",
+            password: "admin"
+        });
+        console.log("Response Token:", responseToken.body);
+        const { token } = responseToken.body;
+
+        const response = await request(app).post("/categories").send(
+            {
+                name: "Category Supertest34",
+                description: "Category Supertest24"
+            }
+        ).set({
+            Authorization: `Bearer ${token}`
+        });
+
+        expect(response.status).toBe(201);
+    });
+
+    it("should not be able to create a duplicate category", async () => {
+        const responseToken = await request(app).post("/sessions").send({
+            email: "admin@email.com",
+            password: "admin"
+        });
+        console.log("Response Token:", responseToken.body);
+        const { token } = responseToken.body;
+
+        await request(app).post("/categories").send(
+            {
+                name: "Category Supertest Duplicate",
+                description: "Category Supertest"
+            }
+        ).set({
+            Authorization: `Bearer ${token}`
+        });
+        const response = await request(app).post("/categories").send(
+            {
+                name: "Category Supertest Duplicate",
+                description: "Category Supertest"
+            }
+        ).set({
+            Authorization: `Bearer ${token}`
+        });
+
+        expect(response.status).toBe(400);
+    });
+});
